1842c55b51b4cb41d3824a44c03112ede5e7973a
VitoRazor <292069198@qq.com>
2020-02-12 13:01:26 +0000

    dir update

--- second/create_data.py
+++ second/create_data.py
@@ -9,9 +9,9 @@ import second.data.nuscenes_dataset as nu_ds
 from second.data.all_dataset import create_groundtruth_database
 
 def kitti_data_prep(root_path):
-    root_path = "G:/3D_detector/data"
-    kitti_ds.create_kitti_info_file(root_path)
-    kitti_ds.create_reduced_point_cloud(root_path)
+    root_path = "/home/xdjf/kitti"
+    #kitti_ds.create_kitti_info_file(root_path)
+    #kitti_ds.create_reduced_point_cloud(root_path)
     create_groundtruth_database("KittiDataset", root_path, Path(root_path) / "kitti_infos_train.pkl")
 
 def nuscenes_data_prep(root_path, version, dataset_name, max_sweeps=10):
@@ -23,4 +23,4 @@ def nuscenes_data_prep(root_path, version, dataset_name, max_sweeps=10):
 
 if __name__ == '__main__':
     #fire.Fire()
-    kitti_data_prep('G:/3D_detector/data')
+    kitti_data_prep('/home/xdjf/kitti')
---
+++ second/start.sh
@@ -0,0 +1,1 @@
+python ./pytorch/train.py train --config_path=./configs/car.fhd.onestage.config --model_dir=./exp/car/onestage_std
